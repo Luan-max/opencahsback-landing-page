c1ff9c641ab19b3ffd0362c1b947ae1b2ba62170
Carlos Eduardo <carloseduardo.and2@gmail.com>
2023-02-07 09:40:07 -0300

    refactor(components/sections): change layout responsive

--- src/components/sections/Impact/index.tsx
+++ src/components/sections/Impact/index.tsx
@@ -5,12 +5,12 @@ export function Impact() {
     <section
       className="
         w-full px-[1.625rem] py-11 md:px-12 md:py-14 lg:px-14 lg:py-16
-        xl:px-16 xl:py-20 2xl:px-[7.5rem] 2xl:py-28 flex flex-col gap-7
-        md:grid md:grid-cols-4 md:gap-3 bg-neutral-900
+        xl:px-16 xl:py-20 2xl:px-[7.5rem] 2xl:py-28 grid justify-center gap-7 
+        md:grid-cols-4 md:gap-3 bg-neutral-900
       "
     >
-      <div className="self-start md:self-auto">
-        <div className="flex self-start items-center gap-5">
+      <div className="flex flex-col items-center">
+        <div className="flex items-center gap-5">
           <span className="text-white font-bold text-2xl lg:text-3xl xl:text-4xl 2xl:text-[2.625rem]">
             +3.5M
           </span>
@@ -24,7 +24,7 @@ export function Impact() {
         </span>
       </div>
 
-      <div className="self-end md:self-auto">
+      <div className="flex flex-col items-center">
         <div className="flex items-center gap-5">
           <span className="text-white font-bold text-2xl lg:text-3xl xl:text-4xl 2xl:text-[2.625rem]">
             +11M
@@ -39,7 +39,7 @@ export function Impact() {
         </span>
       </div>
 
-      <div className="self-start md:self-auto">
+      <div className="flex flex-col items-center">
         <div className="flex items-center gap-5">
           <span className="text-white font-bold text-2xl lg:text-3xl xl:text-4xl 2xl:text-[2.625rem]">
             +8M
@@ -54,7 +54,7 @@ export function Impact() {
         </span>
       </div>
 
-      <div className="self-end md:self-auto">
+      <div className="flex flex-col items-center">
         <div className="flex items-center gap-5">
           <span className="text-white font-bold text-2xl lg:text-3xl xl:text-4xl 2xl:text-[2.625rem]">
             20%
