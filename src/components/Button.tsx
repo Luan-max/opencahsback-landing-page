0b5b259e88da4ff23ece2954cfa4d100a4805afa
Carlos Eduardo <carloseduardo.and2@gmail.com>
2023-02-08 08:58:13 -0300

    refactor(components/button): fix medium button width

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -11,7 +11,7 @@ type ButtonProps = {
 const BUTTON_SIZES_CLASSES = {
   medium: 'w-[10.5rem] h-[2.5rem] text-sm',
   large: 'w-[13.125rem] h-[3.75rem] text-base',
-  full: 'h-[3.75rem]'
+  full: 'w-full h-[3.75rem]'
 }
 
 export function Button({
@@ -25,7 +25,7 @@ export function Button({
       type="button"
       className={cslx(
         `
-        w-full flex justify-center text-brand-secondary bg-brand-main
+        flex justify-center text-brand-secondary bg-brand-main
         font-semibold hover:brightness-90 rounded-lg text-center  
         transition-all ease-in delay-75 items-center gap-2
       `,
