c59216942ba9fa732fc717f5a1bed643f137dd8d
Carlos Eduardo <carloseduardo.and2@gmail.com>
2023-02-09 00:26:52 -0300

    refactor(components/integrators): responsive layout from integrators

--- src/components/sections/Benefits/BenefitsCard.tsx
+++ src/components/sections/Benefits/BenefitsCard.tsx
@@ -7,7 +7,7 @@ export function BenefitsCard({ icon, title }: BenefitsCardProps) {
   return (
     <div className="flex items-center mt-2 space-x-2">
       <div className="flex items-center justify-center">{icon}</div>
-      <span className="text-base font-medium leading-6 text-[#171717] dark:text-gray-200 opacity-[70%]">
+      <span className="text-base font-medium leading-6 text-neutral-900 opacity-[70%]">
         {title}
       </span>
     </div>
--- src/components/sections/Integrators/index.tsx
+++ src/components/sections/Integrators/index.tsx
@@ -1,4 +1,3 @@
-import Container from '@/components/Container'
 import { IntegratorCard } from './IntegratorCard'
 
 import vtex from '@/assets/logos/vtex.svg'
@@ -7,46 +6,63 @@ import shopify from '@/assets/logos/shopify.svg'
 
 export function Integrators() {
   return (
-    <div className="bg-neutral-900">
-      <Container className="flex flex-wrap">
-        <div className="flex items-center w-full lg:w-1/2 lg:py-36 sm:py-0">
-          <div className="mb-20">
-            <h1 className="text-4xl font-semibold text-white lg:text-4xl xl:text-5xl">
-              Integramos com as principais <br /> soluções de mercado
-            </h1>
-            <p className="py-5 text-xl font-medium leading-normal text-white lg:text-xl xl:text-2x opacity-70">
-              A nossa plataforma API first foi feita de forma intuitiva <br />
-              para facilitar a geração de resultado.
-            </p>
-          </div>
-        </div>
-        <div className="flex items-center justify-center w-full lg:w-1/2 mb-14">
-          <IntegratorCard
-            logo={vtex}
-            className="w-40 h-24 sm:w-40 sm:h-40 sm:flex items-center 
-        justify-center bg-gradient-to-r from-neutral-700 to-neutral-800 rounded-2xl cursor-pointer transition transform
-        ease-in delay-75 hover:scale-105 hover:grayscale-0
-        hover:opacity-100"
-            link="https://vtex.com/br-pt/"
-          />
-          <IntegratorCard
-            logo={linx}
-            className="w-40 h-24 sm:w-40 -ml-12 mt-72 sm:h-40 sm:flex items-center 
-            justify-center bg-gradient-to-r from-neutral-700 to-neutral-800 rounded-2xl cursor-pointer transition transform
-            ease-in delay-75 hover:scale-105 hover:grayscale-0 opacity-90
-            hover:opacity-100"
-            link="https://www.linx.com.br/linx-commerce/"
-          />
-          <IntegratorCard
-            logo={shopify}
-            className="w-40 h-24 sm:w-40 sm:h-40 ml-10 mb-16 sm:flex items-center 
-            justify-center bg-gradient-to-r from-neutral-700 to-neutral-800 rounded-2xl cursor-pointer transition transform
-            ease-in delay-75 hover:scale-105 hover:grayscale-0
-            hover:opacity-100"
-            link="https://www.shopify.com/br"
-          />
-        </div>
-      </Container>
-    </div>
+    <section
+      className="
+      bg-neutral-900 flex flex-col lg:flex-row py-16 px-[1.625rem] sm:px-20
+        lg:px-12 lg:gap-20 xl:px-16 xl:py-32 xl:gap-32 2xl:px-[7.5rem] 2xl:py-40
+        2xl:gap-[16rem]
+      "
+    >
+      <div className="flex flex-col gap-4 lg:h-[18rem] lg:justify-center">
+        <h1 className="text-white font-bold text-2xl xl:text-[2.375rem] xl:leading-[3.5rem]">
+          Integramos com as principais soluções de mercado
+        </h1>
+        <span className="text-white opacity-70 font-medium text-sm xl:text-xl xl:leading-[1.875rem]">
+          A nossa plataforma API first foi feita de forma intuitiva para
+          facilitar a geração de resultado.
+        </span>
+      </div>
+
+      <div
+        className="
+          flex items-center justify-center mt-9 lg:mt-0 w-full lg:w-3/4 h-[18rem] 
+          xl:w-[28rem] relative
+        "
+      >
+        <IntegratorCard
+          logo={vtex}
+          className="
+              w-[8.125rem] h-[8.125rem] xl:w-[9.375rem] xl:h-[9.375rem] flex 
+              items-center justify-center bg-gradient-to-r from-neutral-700
+               to-neutral-800 rounded-2xl cursor-pointer transition transform
+              ease-in delay-75 hover:scale-105 hover:grayscale-0 hover:opacity-100
+              absolute left-0 top-0
+            "
+          link="https://vtex.com/br-pt/"
+        />
+        <IntegratorCard
+          logo={linx}
+          className="
+              w-[8.125rem] h-[8.125rem] xl:w-[9.375rem] xl:h-[9.375rem] flex 
+              items-center justify-center bg-gradient-to-r from-neutral-700
+              to-neutral-800 rounded-2xl cursor-pointer transition transform
+              ease-in delay-75 hover:scale-105 hover:grayscale-0 opacity-90
+              hover:opacity-100 absolute left-[6.125rem] bottom-0
+            "
+          link="https://www.linx.com.br/linx-commerce/"
+        />
+        <IntegratorCard
+          logo={shopify}
+          className="
+              w-[8.125rem] h-[8.125rem] xl:w-[9.375rem] xl:h-[9.375rem] flex 
+              items-center justify-center bg-gradient-to-r from-neutral-700 
+              to-neutral-800 rounded-2xl cursor-pointer transition transform
+              ease-in delay-75 hover:scale-105 hover:grayscale-0 hover:opacity-100 
+              absolute left-[12rem] 2xl:top-[-3.625rem]
+            "
+          link="https://www.shopify.com/br"
+        />
+      </div>
+    </section>
   )
 }
